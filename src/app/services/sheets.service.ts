5a71599cd0fa6f431245ba316cad05832f7dd1e4
MatheusMunizera <matheus.munizera@gmail.com>
2022-10-20 20:12:15 -0300

    sheets write

--- src/app/services/sheets.service.ts
+++ src/app/services/sheets.service.ts
@@ -8,36 +8,18 @@ import { FormsViewModel } from '../view-models/forms.view-model';
   providedIn: 'root'
 })
 export class SheetsService {
-  private readonly SHEETS_URL = 'https://sheetsapi.matheusmuniz.dev/sheets';
+  private readonly SHEETS_URL = 'http://sheetsapi.matheusmuniz.dev/sheets';
   
  
   constructor(private http: HttpClient) {}
 
 
   public writeOnSheet(forms : FormsViewModel){
-const teste = {
-  "name": "Matheus Muniz Dantas",
-  "birthdate": "01/05/2001",
-  "gender": "MALE",
-  "sign": "Touro",
-  "cpf": 19497567340,
-  "rg": "27058665X",
-  "father": "Daniel Alexandre Lorenzo da Mota",
-  "mother": "Antônia Camila",
-  "email": "matheus.munizera@gmail.com",
-  "cep": "60181210",
-  "address": "Rua Ponta Mar",
-  "number": "113",
-  "district": "Vicente Pinzon",
-  "city": "Fortaleza",
-  "phone": 85983187119,
-  "height": "1,82",
-  "weight": "86",
-  "blood": "A+",
-  "color": "vermelho"
-}
-const teste2= new SheetsViewModel(forms)
-    this.http.post(`${this.SHEETS_URL}`, teste).subscribe(res => console.log(res))
+       
+    const values = new SheetsViewModel(forms)
+    const body=JSON.stringify(values);
+    this.http.post(`${this.SHEETS_URL}`, body)
+    .subscribe(res => res)
   }
   
    
